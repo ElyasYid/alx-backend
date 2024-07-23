0ad8d048c225f982e0e58149c39976a4bcfc8d3b
Elias Yidnekachew <140306064+ElyasYid@users.noreply.github.com>
2024-07-23 16:28:31 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,30 +1,24 @@
-
 #!/usr/bin/python3
-"""LRU Cache Replacement Implementation Class
-"""
+"""LRU Cache Replacement Implementation Class"""
 from threading import RLock
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    An implementation of LRU(Last Recently Used) Cache
-
+    """implementation of LRU Cache
     Attributes:
         __keys (list): Stores cache keys from least to most accessed
         __rlock (RLock): Lock accessed resources to prevent race condition
     """
     def __init__(self):
-        """ Instantiation method, sets instance attributes
-        """
+        """ Instantiation method"""
         super().__init__()
         self.__keys = []
         self.__rlock = RLock()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """ Add item in cache"""
         if key is not None and item is not None:
             keyOut = self._balance(key)
             with self.__rlock:
@@ -33,8 +27,7 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {}'.format(keyOut))
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """ Get item by key"""
         with self.__rlock:
             value = self.cache_data.get(key, None)
             if key in self.__keys:
@@ -42,8 +35,7 @@ class LRUCache(BaseCaching):
         return value
 
     def _balance(self, keyIn):
-        """ Removes the earliest item from the cache at MAX size
-        """
+        """ Removes earliest item from cache at MAX size"""
         keyOut = None
         with self.__rlock:
             keysLength = len(self.__keys)
@@ -55,4 +47,3 @@ class LRUCache(BaseCaching):
                 self.__keys.remove(keyIn)
             self.__keys.insert(keysLength, keyIn)
         return keyOut
-
